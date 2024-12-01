2e5fb49b923a53097aa8a4affd40aeb2e9ff53fe
robwatsongtr <rwatso@gmail.com>
2024-11-30 20:37:21 -0800

    got something going

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter
+from problems import PROBLEMS
 
 app = FastAPI(
     title="Leetcode Problem Tracker API", openapi_url="/openapi.json"
@@ -7,7 +8,7 @@ app = FastAPI(
 # Used to group API endpoints 
 api_router = APIRouter()
 
-# this is a decorator that registers the root function to define a GET endpoint 
+# this is a decorator that registers the root function to define a GET endpoint.
 @api_router.get("/", status_code=200)
 def root() -> dict:
     """
@@ -15,10 +16,20 @@ def root() -> dict:
     """
     return { "msg": "Server is Running!"}
 
+# the '*' in the parameter list means all parameters after it must be specified by name when called
+@api_router.get("/lcproblems/{problem_id}", status_code=200)
+def fetch_problem(problem_id: int) -> dict:
+    """
+    Fetch single Leetcode problem by ID
+    """
+    result = [ problem for problem in PROBLEMS if problem['id'] == problem_id]
+    if result:
+        return result[0]
+
 # register the api router with the FastAPI object running the show 
 app.include_router(api_router) 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, log_level="debug", reload=True)
 
---
+++ src/problems.py
@@ -0,0 +1,20 @@
+PROBLEMS = [
+    {
+        'id': 1,
+        'lc_num': 27,
+        'problem_name': "Remove Element",
+        'problem_solution': ("""initi left pointer to 0. start for loop with right pointer at 0: if right is the value, skip it and loop to next element ( 'continue' loop), if it's different than value, copy right to left. return left. """).strip()
+    },
+    {
+        'id': 2,
+        'lc_num': 55,
+        'problem_name': "Jump Game",
+        'problem_solution': ("""init furthest to 0. for i in range len nums, first check if current i is greater than furthest, you're stuck so return false. Calculate furthest as the max of current furthest or the value of current index plus the index itself. If furthest is greater than or equal to THE LAST INDEX  return true """)
+    },
+    {
+        'id': 3,
+        'lc_num': 49,
+        'problem_name': "Group Anagrams",
+        'problem_solution': ("""init a hashmap. For word in strs: sort word with "".join(sorted(s)), if sorted word not in hashmap, put it there with an empty array as value. Otherwise every time look up sorted str and append its raw unsorted version from strs array. Outside loop return a list of the hashmap.values""")
+    }
+]

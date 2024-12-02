d327459fd8cb968d7b1e956350aa811fa58faa00
robwatsongtr <rwatso@gmail.com>
2024-12-01 18:25:57 -0800

    add another endpoint

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, APIRouter
 from problems import PROBLEMS
+from typing import Optional
 
 app = FastAPI(
     title="Leetcode Problem Tracker API", openapi_url="/openapi.json"
@@ -22,12 +23,27 @@ def fetch_problem(problem_id: int) -> dict:
     """
     Fetch single Leetcode problem by ID
     """
-    result = [ problem for problem in PROBLEMS if problem['id'] == problem_id]
+    result = [ problem for problem in PROBLEMS if problem['id'] == problem_id ]
     if result:
         return result[0]
+    
+@api_router.get("/search/", status_code=200)
+def search_problems( keyword: Optional[str] = None, max_results: Optional[int] = 10 ) -> dict:
+    """
+    Search for Leetcode problems based on name keyword
+    """
+    if not keyword: 
+        return { "results": PROBLEMS[:max_results] }
+    # filters based on condition of keyword matching in problem_name
+    results = filter(lambda problem: keyword.lower() in problem['problem_name'].lower(), PROBLEMS)
+
+    return { "results": list(results)[:max_results]}
+
+
 
 # register the api router with the FastAPI object running the show 
 app.include_router(api_router) 
+
 
 if __name__ == "__main__":
     import uvicorn

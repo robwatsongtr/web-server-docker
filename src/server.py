ce287ea448a1c8c83c500cbd3b67d9e3a2c65d8a
robwatsongtr <rwatso@gmail.com>
2024-12-03 14:52:03 -0800

    another commit

--- src/server.py
+++ src/server.py
@@ -1,8 +1,37 @@
-from typing import Optional
-from sqlmodel import SQLModel, Field
+from fastapi import FastAPI, APIRouter
+from typing import Annotated
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from contextlib import asynccontextmanager
+
+app = FastAPI(
+    title="Leetcode Problem Tracker API", openapi_url="/openapi.json"
+)
+
+# Used to group API endpoints 
+api_router = APIRouter()
 
 class Problem(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     lc_num: int
     problem_name: str
     problem_solution: str
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+# A Session is what stores the objects in memory and keeps track of any changes
+# needed in the data, then it uses the engine to communicate with the database.
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+# setup the tables on startup 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
